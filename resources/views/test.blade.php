3eef120e2124640453e5d164d1c86cc90bbd0ebc
yasuaki <yasuakihappy2@yahoo.co.jp>
2021-04-27 17:57:30 +0900

    add tailwindcss

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>todo</title>
+    </head>
+    <body>
+        <div class="py-3 px-4 shadow-md rounded-md font-semibold text-white text-base bg-blue-500 ring-2">hoge</div>
+    </body>
+</html>
